a1851d9c0542ff25686482ff503f645c7e783ef3
YasminIbrahiim <yasmeen.yi84@gmail.com>
2024-03-18 03:02:31 +0200

    modifiy parametrized constructor

--- Transaction.cpp
+++ Transaction.cpp
@@ -10,7 +10,25 @@ Transaction:: Transaction(uint8_t  TransactionTypeCpy, uint64_t Transaction_Mone
 {
 	TransactionType   = TransactionTypeCpy;
 	Transaction_Money = Transaction_MoneyCpy;
-	Transaction_Date.year  = Transaction_DateRef.year;
-	Transaction_Date.month = Transaction_DateRef.month;
-	Transaction_Date.day   = Transaction_DateRef.day;
+	/**
+	Member	    Type	Meaning	                    Range
+    tm_sec	    int	    seconds after the minute	0-61*
+    tm_min	    int	    minutes after the hour	    0-59
+    tm_hour	    int	    hours since midnight	    0-23
+    tm_mday	    int	    day of the month	        1-31
+    tm_mon	    int	    months since January	    0-11
+    tm_year	    int	    years since 1900	
+    tm_wday	    int	    days since Sunday	        0-6
+    tm_yday	    int	    days since January 1	    0-365
+    tm_isdst	int	    Daylight Saving Time flag	        **/
+	
+	Transaction_Date.tm_sec	 = Transaction_DateRef.tm_sec  ;	 
+	Transaction_Date.tm_min	 = Transaction_DateRef.tm_min  ; 
+	Transaction_Date.tm_hour = Transaction_DateRef.tm_hour ;
+    Transaction_Date.tm_mday = Transaction_DateRef.tm_mday ;
+    Transaction_Date.tm_mon	 = Transaction_DateRef.tm_mon  ; 
+    Transaction_Date.tm_year = Transaction_DateRef.tm_year ;
+    Transaction_Date.tm_wday = Transaction_DateRef.tm_wday ;
+    Transaction_Date.tm_yday = Transaction_DateRef.tm_yday ;
+    Transaction_Date.tm_isdst= Transaction_DateRef.tm_isdst;	
 }
